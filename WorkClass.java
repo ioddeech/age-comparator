f8c7041049857e164cb0efd9a0f41580d4d34ff3
MIKITA IODA <nikitaiodo201@gmail.com>
2022-12-04 13:53:34 +0300

    начало работы над окончаниями

--- WorkClass.java
+++ WorkClass.java
@@ -1,7 +1,15 @@
 public class WorkClass {
     public double srAge = 0;
+    String let = " лет.";
+    String god = " год.";
+    String goda = " года.";
 
     public double getSrAge(int min, int max, int middle) {
         return (middle+max+min)/3;
+    }
+
+    public void ageCompiler(int min, int max, int middle){
+        String smin =
+
     }
 }
