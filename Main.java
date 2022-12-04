bf8c39c5dcae8705e3fbdc50a7caf932fa6a8887
MIKITA IODA <nikitaiodo201@gmail.com>
2022-12-04 22:51:56 +0300

    вынос вычислений в класс WorkClass.java

--- Main.java
+++ Main.java
@@ -13,45 +13,10 @@ public class Main {
         System.out.println("Введите возраст Миши:");
         int mishaAge = new Scanner(System.in).nextInt();
 
-        int min = -1;
-        int middle = -1;
-        int max = -1;
-
-        if ((vasyaAge > 120 || vasyaAge < 0) ||
-                (katyaAge > 120 || katyaAge < 0) ||
-                (mishaAge > 120 || mishaAge < 0)) {
-            System.out.println("Неправильное значение. Повторите попытку.");
-        } else {
-            //maxAge
-            if (vasyaAge >= katyaAge && vasyaAge >= mishaAge) {
-                max = vasyaAge;
-            } else if (katyaAge >= vasyaAge && katyaAge >= mishaAge) {
-                max = katyaAge;
-            } else if (mishaAge >= katyaAge && mishaAge >= vasyaAge) {
-                max = mishaAge;
-            }
-
-            //middleAge
-            if ((vasyaAge >= katyaAge || vasyaAge >= mishaAge) &&
-                    (vasyaAge <= katyaAge || vasyaAge <= mishaAge)) {
-                middle = vasyaAge;
-            } else if ((mishaAge >= katyaAge || mishaAge >= vasyaAge) &&
-                    (mishaAge <= katyaAge || mishaAge <= vasyaAge)) {
-                middle = mishaAge;
-            } else if ((katyaAge >= mishaAge || katyaAge >= vasyaAge) &&
-                    (katyaAge <= mishaAge || katyaAge <= vasyaAge)) {
-                middle = katyaAge;
-            }
-
-            //minAge
-            if (vasyaAge <= katyaAge && vasyaAge <= mishaAge) {
-                min = vasyaAge;
-            } else if (katyaAge <= vasyaAge && katyaAge <= mishaAge) {
-                min = katyaAge;
-            } else if (mishaAge <= katyaAge && mishaAge <= vasyaAge) {
-                min = mishaAge;
-            }
-
+        int max = workClass.maxAgeCalcs(vasyaAge,katyaAge,mishaAge);;
+        int min = workClass.minAgeCalcs(vasyaAge,katyaAge,mishaAge);;
+        int middle = workClass.middleAgeCalcs(vasyaAge,katyaAge,mishaAge);;
+        workClass.maxAgeCalcs(vasyaAge,katyaAge,mishaAge);
             //printing
             System.out.println("Наименьший возраст: " + min + workClass.getAgeEnd(min));
             System.out.println("Средний возраст: " + middle + workClass.getAgeEnd(middle));
@@ -60,4 +25,4 @@ public class Main {
             System.out.println("Среднее значение среди всех: " + workClass.getSrAge(min, max, middle));
         }
     }
-}
+
--- WorkClass.java
+++ WorkClass.java
@@ -1,8 +1,9 @@
 public class WorkClass {
     public double srAge = 0;
-    String let = " лет.";
-    String god = " год.";
-    String goda = " года.";
+
+    public int min;
+    public int max;
+    public int middle;
 
     public double getSrAge(int min, int max, int middle) {
         return (middle+max+min)/3;
@@ -20,4 +21,43 @@ public class WorkClass {
         }
             return " года";
     }
+
+
+
+    public int maxAgeCalcs(int vasyaAge, int katyaAge, int mishaAge){
+        if (vasyaAge >= katyaAge && vasyaAge >= mishaAge) {
+            max = vasyaAge;
+        } else if (katyaAge >= vasyaAge && katyaAge >= mishaAge) {
+            max = katyaAge;
+        } else if (mishaAge >= katyaAge && mishaAge >= vasyaAge) {
+            max = mishaAge;
+        }
+        return max;
+    }
+
+    public int middleAgeCalcs(int vasyaAge, int katyaAge, int mishaAge){
+        if ((vasyaAge >= katyaAge || vasyaAge >= mishaAge) &&
+                (vasyaAge <= katyaAge || vasyaAge <= mishaAge)) {
+            middle = vasyaAge;
+        } else if ((mishaAge >= katyaAge || mishaAge >= vasyaAge) &&
+                (mishaAge <= katyaAge || mishaAge <= vasyaAge)) {
+            middle = mishaAge;
+        } else if ((katyaAge >= mishaAge || katyaAge >= vasyaAge) &&
+                (katyaAge <= mishaAge || katyaAge <= vasyaAge)) {
+            middle = katyaAge;
+        }
+        return middle;
+    }
+
+    public int minAgeCalcs(int vasyaAge, int katyaAge, int mishaAge){
+        if (vasyaAge <= katyaAge && vasyaAge <= mishaAge) {
+            min = vasyaAge;
+        } else if (katyaAge <= vasyaAge && katyaAge <= mishaAge) {
+            min = katyaAge;
+        } else if (mishaAge <= katyaAge && mishaAge <= vasyaAge) {
+            min = mishaAge;
+        }
+        return min;
+    }
 }
+
